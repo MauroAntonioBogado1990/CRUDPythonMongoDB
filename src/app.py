b4ed59bc8ccd759feaf8549940bb714bd35f1c5c
Mauro Bogado <mauroantoniobogado@gmail.com>
2021-06-16 11:58:54 -0300

    Agregado de entorno virtual

--- src/app.py
+++ src/app.py
@@ -1,9 +1,20 @@
-# importamos Flask
 from flask import Flask
-
-
+from flask_pymongo import PyMongo
 # paso una instancia de la aplicación
-app = flask(__name__)
+app = Flask(__name__)
+# propiedades de la DB
+app.config['MONGO_URI'] = 'mongodb://localhost/pythonMongoDB'
+
+# realizamos la conección
+mongo = PyMongo(app)
+# creamos la ruta- Crear usuarios
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    # Recibiendo datos
+    return {'message': 'received'}
+
 
 # Ejecutamos como principal
 if __name__ == "__main__":
